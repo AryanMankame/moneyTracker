239c2185b99b48570359eca74204221a2d5a0094
Aryan Mankame <aryan672002@gmail.com>
2023-02-01 13:56:25 +0530

    1/2/23

--- ChatApplication/chatapplication/src/components/Home.js
+++ ChatApplication/chatapplication/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import styled from 'styled-components';
 import {io} from 'socket.io-client'; 
 const ChatContainer = styled.div`
@@ -51,16 +51,30 @@ const ChatButton = styled.button`
     background-color: #004d4d;
   }
 `;
-
+var m = 0
+const socket = io("http://localhost:3002");
 function ChatApp() {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
-  const socket = io("http://localhost:3002");
-  socket.on('connection',(socket) => {
-    setMessages("Im on...",socket.id);
-  })
+  // const [id,setid] = useState(0);
+  useEffect(() => {
+      if(m > 0) return ;
+      socket.on('connect',() => {
+        console.log("Connected" + socket.id);
+        // m.push(socket.id);
+        setMessages([...messages,"Im on... " + socket.id]);
+      })
+      socket.on('new_inserted',(data) =>{
+        // alert(socket.id)
+        // m.push(data)
+        setMessages(message => [...message,data])
+      })
+      m += 1
+  },[])
   const handleSend = () => {
-    setMessages([...messages, input]);
+    setMessages(message => [...message,input]);
+    // m.push(input)
+    socket.emit('check',input)
     setInput('');
   };
 
